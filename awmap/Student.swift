e714238bf816956855836f1f775ed5ea573c9bef
Andree Wijaya <andreewijaya09@gmail.com>
2016-03-29 10:40:48 +0700

    - managed to get student list from Parse - created Student object model - added methods inside Student object to parse student object from dictionary

---
+++ awmap/Student.swift
@@ -0,0 +1,48 @@
+//
+//  Student.swift
+//  awmap
+//
+//  Created by Andree Wijaya on 3/29/16.
+//  Copyright © 2016 Andree Wijaya. All rights reserved.
+//
+
+import Foundation
+
+class Student {
+    
+    var firstName: String
+    var lastName: String
+    var latitude: Float
+    var longitude: Float
+    var mapString: String
+    var mediaURL: String
+    var objectId: String
+    var uniqueKey: String
+    var createdAt: NSString
+    var updatedAt: NSString
+    
+    //constructing student from dictionary
+    init(dictionary: [String:AnyObject]){
+        firstName = dictionary["firstName"] as! String
+        lastName = dictionary["lastName"] as! String
+        latitude = dictionary["latitude"] as! Float
+        longitude = dictionary["longitude"] as! Float
+        mapString = dictionary["mapString"] as! String
+        mediaURL = dictionary["mediaURL"] as! String
+        objectId = dictionary["objectId"] as! String
+        uniqueKey = dictionary["uniqueKey"] as! String
+        createdAt = dictionary["createdAt"] as! NSString
+        updatedAt = dictionary["updatedAt"] as! NSString
+        
+    }
+    
+    static func studentFromResult(results: [[String: AnyObject]]) -> [Student]{
+        var students = [Student]()
+        
+        for result in results {
+            students.append(Student(dictionary: result))
+        }
+        
+        return students
+    }
+}
--- awmap/UserListTableViewController.swift
+++ awmap/UserListTableViewController.swift
@@ -10,15 +10,31 @@ import UIKit
 
 class UserListTableViewController: UITableViewController {
     let cellIdentifier = "studentCell"
+    @IBOutlet var studentListTableView: UITableView!
     
-    let studentList = [["firstName": "Jarrod", "lastName": "Parkes", "objectID": "JhOtcRkxsh", "latitude": 34.7303688, "longitude": -86.5861037, "mediaURL": "https://www.linkedin.com/in/jarrodparkes"],
-                       ["firstName": "Jessica", "lastName": "Uelmen", "objectID": "kj18GEaWD8", "latitude": 28.1461248, "longitude": -82.756768, "mediaURL": "www.linkedin.com/in/jessicauelmen/en"]]
+    
+    var studentList1 = [Student]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
       
         }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        getStudentList { (result, error) in
+            guard (result != nil || error == nil) else {
+                print("Unable to get student list")
+                return
+            }
+            
+            self.studentList1 = result!
+            performUpdateOnMain({ 
+                self.studentListTableView.reloadData()
+            })
+        }
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -33,28 +49,16 @@ class UserListTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return studentList.count
+        return studentList1.count
     }
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier, forIndexPath: indexPath)
-        var studentName = ""
-        let student = studentList[indexPath.row] as NSDictionary
-        if let firstName = student["firstName"] as? String {
-            if let lastName = student["lastName"] as? String {
-                studentName = firstName + " " + lastName
-                cell.textLabel?.text = studentName
-            }
-        }
-        if let mediaURL = student["mediaURL"] as? String {
-            print(mediaURL)
-            cell.detailTextLabel?.text = mediaURL
-            
-        }else {
-            "unable to find mediaURL"
-        }
+        let student = studentList1[indexPath.row]
         
+        cell.textLabel?.text = student.firstName
+        cell.detailTextLabel?.text = student.mediaURL
         cell.imageView?.image = UIImage(named: "pin")
         
         return cell
@@ -74,8 +78,45 @@ class UserListTableViewController: UITableViewController {
             UIApplication.sharedApplication().openURL(referenceURL)
         }
 
-        
-
+    }
+    
+    func getStudentList(completionHandlerForStudentList: (result: [Student]?, error: NSError?)-> Void){
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://api.parse.com/1/classes/StudentLocation")!)
+        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
+        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil { // Handle error...
+                completionHandlerForStudentList(result: nil, error: error)
+                return
+            }
+            
+            guard let data = data else{
+                completionHandlerForStudentList(result: nil, error: error)
+                return
+            }
+            
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else{
+                completionHandlerForStudentList(result: nil, error: error)
+                return
+            }
+            
+            var parsedResult: AnyObject
+            do{
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            }catch{
+                return
+            }
+            
+            guard let resultArray = parsedResult["results"] as? [[String: AnyObject]] else{
+                completionHandlerForStudentList(result: nil, error: error)
+                return
+            }
+            
+            let students = Student.studentFromResult(resultArray)
+            completionHandlerForStudentList(result: students, error: nil)
+        }
+        task.resume()
     }
 
 
